4103599761c01077211673dcb7bf553b3af409c4
RD <ruydelgado@gmail.com>
2022-08-12 21:26:33 -0500

    ..

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -3,7 +3,7 @@
 const static size_t NUM_COLS = 80;
 const static size_t NUM_ROWS = 25;
 
-struct Char { 
+struct Char {
     uint8_t character;
     uint8_t color;
 };
@@ -13,64 +13,71 @@ size_t col = 0;
 size_t row = 0;
 uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
 
-void clear_row(size_t row) { 
-    struct Char empty = (struct Char) { 
+void clear_row(size_t row) {
+    struct Char empty = (struct Char) {
         character: ' ',
-        color: color
+        color: color,
     };
-    for (size_t col = 0; col < NUM_COLS; col++) { 
-        buffer[col + NUM_COLS *  row] = empty;
+
+    for (size_t col = 0; col < NUM_COLS; col++) {
+        buffer[col + NUM_COLS * row] = empty;
     }
 }
 
-void print_clear() { 
-    for (size_t i = 0; i < NUM_ROWS * NUM_COLS; i++) { 
+void print_clear() {
+    for (size_t i = 0; i < NUM_ROWS; i++) {
         clear_row(i);
     }
 }
 
-void print_newline() { 
+void print_newline() {
     col = 0;
 
-    if (row < NUM_ROWS - 1) { 
+    if (row < NUM_ROWS - 1) {
         row++;
         return;
     }
 
-    for (size_t row = 1; row < NUM_ROWS; row++) { 
-        for (size_t col = 0; col < NUM_COLS; col++) { 
+    for (size_t row = 1; row < NUM_ROWS; row++) {
+        for (size_t col = 0; col < NUM_COLS; col++) {
             struct Char character = buffer[col + NUM_COLS * row];
             buffer[col + NUM_COLS * (row - 1)] = character;
         }
     }
 
-    clear_row(NUM_ROWS - 1);
+    clear_row(NUM_COLS - 1);
 }
 
 void print_char(char character) {
-    if (character == '\n') { 
+    if (character == '\n') {
         print_newline();
         return;
     }
-    if (col > NUM_COLS) { 
+
+    if (col > NUM_COLS) {
         print_newline();
     }
 
-     buffer[col + NUM_COLS * row] = (struct Char) { 
+    buffer[col + NUM_COLS * row] = (struct Char) {
         character: (uint8_t) character,
         color: color,
-     };
-     col++;
+    };
+
+    col++;
 }
 
-void print_str(char* str) { 
-    for(size_t i =0; str[i] != '\0'; i++) {
+void print_str(char* str) {
+    for (size_t i = 0; 1; i++) {
         char character = (uint8_t) str[i];
+
+        if (character == '\0') {
+            return;
+        }
+
         print_char(character);
     }
 }
 
-
-void print_set_color(uint8_t fg, uint8_t bg) { 
-    color = fg + (bg << 4);
+void print_set_color(uint8_t foreground, uint8_t background) {
+    color = foreground + (background << 4);
 }
