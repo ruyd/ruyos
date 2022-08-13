f3e093125540f6444e23ccd5ef685d44a58287d1
RD <ruydelgado@gmail.com>
2022-08-12 21:19:04 -0500

    ..

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -1,5 +1,5 @@
 global long_mode_start
-
+extern kernel_main
 
 section .text
 bits 64
@@ -12,7 +12,5 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 
-    ; print ok
-    mov dword [0xb8000], 0x2f4b2f4f
-
+        call kernel_main
     hlt
